b538f6a7452698b57d2828076f8f000144106c5b
Juhyun <boochickenood@gmail.com>
2022-12-03 22:51:08 +0900

    Feat: Redirect시 사용할 Redirect 함수 구현
    현재 직접적으로 사용하지는 않지만 후에 redirect 관련 코드를 빠르게 작성하기 위해 구현했습니다'

---
+++ src/router/Redirect.ts
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { To, useNavigate } from "react-router-dom";
+
+function Redirect({ to }: { to: To }) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate(to);
+  });
+
+  return null;
+}
+
+export default Redirect;
